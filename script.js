99530d4811afc0016dcd91af266b92f217cd5863
Anshu <kushwaha.anshu8@gmail.com>
2025-06-20 11:59:25 +0530

    e commerce assignment completed with local Storage and remove cart

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
     { id: 4, name: "product 4", price: 54.99 },
   ];
 
-  const cart = [];
-
   const productList = document.getElementById("product-list");
 
   const cartItems = document.getElementById("cart-items");
@@ -17,83 +15,120 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
   const cartTotal = document.getElementById("cart-total");
 
+  let cartData = localStorage.getItem("cart");
+  let savedCart = [];
+
+  if (cartData) {
+    try {
+      savedCart = JSON.parse(cartData);
+    } catch (e) {
+      console.error("Failed to parse cart data:", e);
+      savedCart = [];
+    }
+  }
+  let totalPrice = 0;
+
+  savedCart.forEach((cart) => renderTotal(cart));
+
+  // Display Products
   products.forEach((product) => {
     const productDiv = document.createElement("div");
     productDiv.classList.add("product");
     productDiv.innerHTML = `
-        <span>${product.name} - $${product.price.toFixed(2)} </span>
+        <span>${product.name} - $${product.price} </span>
         <button data-id="${product.id}">Add to cart</button>
     `;
     productList.appendChild(productDiv);
   });
 
+  //  Add products to cart
   productList.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
       e.preventDefault();
-      //   console.log("clicked");
+
+      // Get product ID
       const productId = parseInt(e.target.getAttribute("data-id"));
 
+      // Match attribute Id with product Id in array
       const product = products.find((p) => p.id === productId);
 
-      addToCart(product);
-    }
-  });
-
-  function addToCart(product) {
-    // console.log(product);
-    cart.push(product);
-    renderTotal();
-  }
-
-  function renderTotal() {
-    cartItems.innerText = "";
-
-    let totalPrice = 0;
-    if (cart.length > 0) {
-      cartTotal.classList.remove("hidden");
-
-      cart.forEach((item, index) => {
-        totalPrice += item.price;
-        // console.log(item);
-        const totalElement = document.createElement("div");
-        totalElement.classList.add("cart-list");
-        totalElement.innerHTML = `${item.name} - $${item.price.toFixed(
-          2
-        )} <button class="remove" data-id="${item.id}">Remove</button>`;
-
-        totalInput.textContent = `${totalPrice.toFixed(2)}`;
-        cartItems.append(totalElement);
+      let productAdded = false;
+      // Duplicate products should not be added
+      savedCart.forEach((item) => {
+        if (productId === item.id) {
+          productAdded = true;
+        }
       });
-    } else {
-      cartTotal.classList.add("hidden");
-    }
-  }
 
-  // Remove product from cart
-  const removeButton = document.getElementById("cart-items");
-
-  removeButton.addEventListener("click", (e) => {
-    if (e.target.tagName === "BUTTON") {
-      const cartId = parseInt(e.target.getAttribute("data-id"));
-
-      const itemId = cart.find((c) => c.id === cartId);
-
-      removeFromCart(itemId);
+      // If product is already added then show alert message
+      if (productAdded) {
+        return alert("product already added");
+      } else {
+        // Else push the product in cart and save in local storage + render
+        savedCart.push(product);
+        renderTotal(product);
+        saveCart();
+      }
     }
   });
 
-  function removeFromCart(itemId) {
-    const index = cart.findIndex((obj) => obj === itemId);
-    if (index !== -1) {
-      cart.splice(index, 1);
-    }
-    renderTotal();
+  function renderTotal(product) {
+    // Hide empty cart message
+    emptyInput.classList.add("hidden");
+
+    // Show cart items
+    cartTotal.classList.remove("hidden");
+
+    // Calculate total price
+    totalPrice += product.price;
+
+    // Creat a new div element
+    const totalElement = document.createElement("div");
+    totalElement.classList.add("cart-list");
+    totalElement.innerHTML = `${product.name} - $${product.price} <button class="remove" data-id="${product.id}">Remove</button>`;
+
+    // Show Price
+    totalInput.textContent = `${totalPrice.toFixed(2)}`;
+
+    // Append Cart list in Cart div
+    cartItems.append(totalElement);
+
+    totalElement
+      .querySelector("button")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+
+        // Get the product id to remove
+        const cartId = parseInt(e.target.getAttribute("data-id"));
+
+        // Get the cart items after remove that product
+        savedCart = savedCart.filter((c) => c.id !== cartId);
+
+        totalPrice = 0;
+
+        // CALCULATE NEW PRICE
+        savedCart.forEach((item) => (totalPrice = totalPrice + item.price));
+
+        // remove the div of deleted product
+        totalElement.remove();
+
+        // Show final price
+        totalInput.textContent = `${totalPrice.toFixed(2)}`;
+
+        // Save the cart in local Storage
+        saveCart();
+      });
+  }
+
+  function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(savedCart));
   }
 
   checkoutButton.addEventListener("click", function (e) {
     cartItems.innerText = "";
-    cart.length = 0;
+    savedCart.length = 0;
     alert("Checkout successfull");
-    renderTotal();
+    localStorage.setItem("cart", "");
+    totalPrice = 0;
   });
 });
